5afad3b97eba03a4fd8a34a242dc1d28dfd42b8c
etsraphael <ets_raphael@outlook.fr>
2020-03-29 20:37:12 +1000

    printed data in boucle

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Search from './Components/Search';
+
+export default class App extends React.Component {
+  render() { return (<Search />) }
+}
---
+++ Components/Search.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, TextInput, Button, FlatList, StyleSheet, Text } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem';
+
+class Search extends React.Component {
+    render() {
+        return (
+            <View style={styles.main_container}>
+                <TextInput placeholder="Titre du film" style={styles.textInput}></TextInput>
+                <Button style={styles.searchBtn} title="Rechercher" onPress={() => { }} ></Button>
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => <FilmItem film={item} />}
+                />
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    main_container: {
+        marginTop: 20,
+        flex: 1
+    },
+    textInput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        paddingLeft: 5,
+        borderWidth: 1
+    },
+    searchBtn: {
+        height: 50
+    }
+});
+
+export default Search
