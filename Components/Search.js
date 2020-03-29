4f0f46d7958b99a80eb492d36b9288f0539f360c
etsraphael <ets_raphael@outlook.fr>
2020-03-29 22:22:18 +1000

    get movie with my apis

--- Components/Search.js
+++ Components/Search.js
@@ -2,15 +2,33 @@ import React from 'react'
 import { View, TextInput, Button, FlatList, StyleSheet, Text } from 'react-native'
 import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem';
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { films: [] }
+      }
+
+    _loadFilms() {
+        getFilmsFromApiWithSearchedText("star").then(data => {
+            this.setState({films: data.results})
+          })
+    }
+
     render() {
         return (
             <View style={styles.main_container}>
                 <TextInput placeholder="Titre du film" style={styles.textInput}></TextInput>
-                <Button style={styles.searchBtn} title="Rechercher" onPress={() => { }} ></Button>
+                <Button
+                    style={styles.searchBtn}
+                    title="Rechercher"
+                    onPress={() => this._loadFilms()}
+                >
+                </Button>
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => <FilmItem film={item} />}
                 />
